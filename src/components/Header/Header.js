496c41699087140653fcdb2e060132dd598b0a8d
Giga Uchaneishvili <g.uchaneishvili@gmail.com>
2023-02-07 22:23:20 +0100

    clean code

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,15 +3,7 @@
 import React from 'react';
 import styles from './Header.module.css';
 function Header() {
-	return (
-		<div className={styles.nav}>
-			<div
-				className={styles.navBtn}
-				id='nav-check'></div>
-
-			<div className={styles.navLink}></div>
-		</div>
-	);
+	return <div className={styles.nav}></div>;
 }
 
 export default Header;
