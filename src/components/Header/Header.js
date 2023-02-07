b2e5979f6288f34b4cc2a43856086e9792bb00c1
Giga Uchaneishvili <g.uchaneishvili@gmail.com>
2023-02-08 00:21:56 +0100

    positions of footer and padding of header is fixed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,17 @@
 
 import React from 'react';
 import styles from './Header.module.css';
+import { MenuOutlined } from '@ant-design/icons';
+
 function Header() {
-	return <div className={styles.nav}></div>;
+	return (
+		<div className={styles.nav}>
+			<div className={styles.logoContainer}>Kartu Verbs</div>
+			<div className={styles.menuContainer}>
+				<MenuOutlined />
+			</div>
+		</div>
+	);
 }
 
 export default Header;
