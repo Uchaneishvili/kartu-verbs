2b90f77a7dfd4ee21ed47900e99509e7627c0397
bachoSa <bachosaghinadze@yahoo.com>
2023-02-14 03:10:22 +0100

    +mainPage + searchBar + shadows to all

--- src/pages/Mainpage/Mainpage.js
+++ src/pages/Mainpage/Mainpage.js
@@ -4,12 +4,16 @@ import React from "react";
 import Page from "../../components/page";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import SearchBar from "../../components/MainContent/SearchBar.js";
+
 function Mainpage() {
   return (
     <>
       <Header />
 
-      <Page></Page>
+      <Page>
+        <SearchBar />
+      </Page>
 
       <Footer />
     </>
