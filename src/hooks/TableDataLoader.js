66da02b133dbebf7c601c41490f2396dae0989f0
Giga Uchaneishvili <g.uchaneishvili@gmail.com>
2023-03-21 23:11:41 +0100

    parser code refactoring

---
+++ src/hooks/TableDataLoader.js
@@ -0,0 +1,50 @@
+/** @format */
+
+import axios from 'axios';
+import { useState, useEffect, useCallback } from 'react';
+
+export function useList(url, query) {
+	const [list, setList] = useState([]);
+
+	const getData = useCallback(async () => {
+		try {
+			const response = await axios.get(url, {
+				params: {
+					query: query,
+				},
+			});
+
+			// split the table string into rows
+			let rows = response.data.split('\n');
+
+			// extract headers from the second row
+			let headers = rows[1]
+				.split('|')
+				.slice(1, -1)
+				.map((header) => header.trim());
+
+			// create an array of objects for the table data
+			let tableData = [];
+			for (let i = 3; i < rows.length - 2; i++) {
+				let rowData = rows[i]
+					.split('|')
+					.slice(1, -1)
+					.map((cell) => cell.trim());
+				let obj = {};
+				for (let j = 0; j < headers.length; j++) {
+					obj[headers[j]] = rowData[j];
+				}
+				tableData.push(obj);
+				setList(tableData);
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	}, [query, url]);
+
+	useEffect(() => {
+		getData();
+	}, [getData]);
+
+	return [list];
+}
--- src/pages/Mainpage/Mainpage.js
+++ src/pages/Mainpage/Mainpage.js
@@ -4,15 +4,34 @@ import React from 'react';
 import Page from '../../components/page';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
-import SearchBar from '../../components/SearchBar/SearchBar.js';
+import { Table } from 'antd';
+import { useList } from '../../hooks/TableDataLoader.js';
 
 function Mainpage() {
+	const [list] = useList(
+		'http://servolis.irisa.fr:3737/kartuverbs/sparql',
+		'PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX n1: <file:///home/ferre/data/ontologies/Kartu-verbs/> SELECT ?u ?l ?p WHERE { VALUES ?u { <file:///home/ferre/data/ontologies/Kartu-verbs/დარდობდეთ> <file:///home/ferre/data/ontologies/Kartu-verbs/ვიჩქარებთ> <file:///home/ferre/data/ontologies/Kartu-verbs/თავაზობდით> } }'
+	);
+
+	const columns = [
+		{
+			title: 'erpId',
+			sorter: true,
+			dataIndex: 'u',
+		},
+	];
+
 	return (
 		<>
 			<Header search={true} />
 
 			<Page>
-				<SearchBar />
+				{/* <SearchBar /> */}
+
+				<Table
+					columns={columns}
+					dataSource={list}
+				/>
 			</Page>
 
 			<Footer />
