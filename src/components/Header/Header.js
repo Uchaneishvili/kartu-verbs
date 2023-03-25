40d21270e939e1a10ecd534c660341942665e68c
bachoSa <bachosaghinadze@yahoo.com>
2023-03-26 00:27:25 +0100

    nav to homepage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,40 +1,51 @@
 /** @format */
 
-import React, { useState } from 'react';
-import styles from './Header.module.css';
-import { MenuOutlined } from '@ant-design/icons';
-import { SearchBarIcon } from '../../icons/searchBarIcon';
-import { Form } from 'antd';
+import React, { useState } from "react";
+import styles from "./Header.module.css";
+import { MenuOutlined } from "@ant-design/icons";
+import { SearchBarIcon } from "../../icons/searchBarIcon";
+import { Form } from "antd";
+
+import { useNavigate } from "react-router-dom";
+
 
 function Header(props) {
-	const [searchValue, setSearchValue] = useState('');
-	return (
-		<div className={styles.nav}>
-			<div className={styles.logoContainer}>Kartu Verbs</div>
-			{props.search && (
-				<div className={styles.searchBarContainer}>
-					<Form className={styles.searchForm}>
-						<input
-							className={styles.searchInput}
-							type='text'
-							defaultValue={searchValue}
-							onChange={(e) => setSearchValue(e.target.value)}
-							placeholder='Search any word'
-						/>
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
+  return (
+    <div className={styles.nav}>
+      <div className={styles.logoContainer}>
+        {" "}
+        <span
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          Kartu Verbs
+        </span>{" "}
+      </div>
+      {props.search && (
+        <div className={styles.searchBarContainer}>
+          <Form className={styles.searchForm}>
+            <input
+              className={styles.searchInput}
+              type="text"
+              defaultValue={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+              placeholder="Search any word"
+            />
 
-						<button
-							type='submit'
-							className={styles.searchButton}>
-							<SearchBarIcon />
-						</button>
-					</Form>
-				</div>
-			)}
-			<div className={styles.menuContainer}>
-				<MenuOutlined />
-			</div>
-		</div>
-	);
+            <button type="submit" className={styles.searchButton}>
+              <SearchBarIcon />
+            </button>
+          </Form>
+        </div>
+      )}
+      <div className={styles.menuContainer}>
+        <MenuOutlined />
+      </div>
+    </div>
+  );
 }
 
 export default Header;
