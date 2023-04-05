77b1f188721af27015bedc33336e797903d76b55
bachoSa <bachosaghinadze@yahoo.com>
2023-04-06 00:14:33 +0200

    AllVerb

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
 /** @format */
 
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Mainpage from './pages/Mainpage/Mainpage';
-import DetailPage from './pages/DetailPage/DetailPage';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Mainpage from "./pages/Mainpage/Mainpage";
+import DetailPage from "./pages/DetailPage/DetailPage";
+import Allverb from "./pages/Allverb/Allverb";
 
 function App() {
-	return (
-		<div>
-			<BrowserRouter>
-				<Routes>
-					<Route
-						path='/'
-						exact
-						element={<Mainpage />}
-					/>
-					<Route
-						path='/DetailPage'
-						exact
-						element={<DetailPage />}
-					/>
-				</Routes>
-			</BrowserRouter>
-		</div>
-	);
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Mainpage />} />
+          <Route path="/DetailPage" exact element={<DetailPage />} />
+          <Route path="/Allverb" exact element={<Allverb />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Allverb/Allverb.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Table, Row } from "antd";
+import { useList } from "../../hooks/TableDataLoader.js";
+import Page from "../../components/page";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+
+const Allverb = () => {
+  const [list] = useList(
+    `PREFIX n1: <file:///home/ferre/data/ontologies/Kartu-verbs/>
+    SELECT DISTINCT ?Georgian_infinitive_105 ?English_infinitive_1
+    WHERE { ?thing_4 n1:English_infinitive ?English_infinitive_1 .
+            ?thing_4 n1:Georgian_infinitive ?Georgian_infinitive_105 . }
+    LIMIT 6`
+  );
+
+  console.log(list);
+  const columns = [
+    {
+      title: "Georgian Infinitive",
+      dataIndex: "Georgian_infinitive_105",
+      key: "geo",
+      render: (val) => {
+        return val?.slice(3);
+      },
+    },
+    {
+      title: "English Infinitive",
+      dataIndex: "English_infinitive_1",
+      key: "eng",
+      render: (val) => {
+        return val?.slice(3);
+      },
+    },
+  ];
+
+  return (
+    <Page>
+      <Header />
+      <Row style={{ justifyContent: "center" }}>
+        <Table
+          size="medium"
+          bordered={false}
+          columns={columns}
+          dataSource={list}
+          pagination={false}
+          rowKey={(record) => record.verb_1}
+        />
+      </Row>
+      <Footer />
+    </Page>
+  );
+};
+
+export default Allverb;
