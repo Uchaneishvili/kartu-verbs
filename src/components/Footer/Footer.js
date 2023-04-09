26e31397b250a8b93147991ad2fe20166f9464e4
Giga Uchaneishvili <g.uchaneishvili@gmail.com>
2023-04-09 18:37:52 +0200

    footer is fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,22 +1,24 @@
-import React from "react";
+/** @format */
 
-import styles from "./Footer.module.css";
+import React from 'react';
+
+import styles from './Footer.module.css';
 
 const Footer = () => {
-  return (
-    <div className={styles.StyledContainer}>
-      <div className={styles.StyledWrapper}>
-        <ul>
-          <li>About us</li>
-          <li>Authorization</li>
-          <li>Terms and Conditions</li>
-        </ul>
-        <ul>
-          <li>Contact & FAQ</li>
-        </ul>
-      </div>
-    </div>
-  );
+	return (
+		<footer className={styles.StyledContainer}>
+			<div className={styles.StyledWrapper}>
+				<ul>
+					<li>About us</li>
+					<li>Authorization</li>
+					<li>Terms and Conditions</li>
+				</ul>
+				<ul>
+					<li>Contact & FAQ</li>
+				</ul>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
