bdeef5ab4a8f4a46d2b2a0c4a37ef0de2678b2f2
Giga Uchaneishvili <g.uchaneishvili@gmail.com>
2023-04-12 02:41:18 +0200

    font is imported

--- src/pages/Mainpage/Mainpage.js
+++ src/pages/Mainpage/Mainpage.js
@@ -1,46 +1,46 @@
 /** @format */
 
-import React from "react";
-import Page from "../../components/page";
-import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer";
-import SearchBar from "../../components/SearchBar/SearchBar";
+import React from 'react';
+import Page from '../../components/page';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import SearchBar from '../../components/SearchBar/SearchBar';
 
-import { MainTitle } from "../../icons/mainTitle";
+import { MainTitle } from '../../icons/mainTitle';
 
-import styles from "./MainPage.module.css";
+import styles from './MainPage.module.css';
 
 function Mainpage() {
-  return (
-    <>
-      <Header search={false} />
+	return (
+		<>
+			<Header search={false} />
 
-      <Page>
-        <main>
-          <div>
-            <div className={styles.mainTitleContainer}>
-              <div style={{ width: "350px" }}>
-                <MainTitle />
-              </div>
-            </div>
-          </div>
-          <div className={styles.searchContainer}>
-            <div className={styles.searchTitleContainer}>
-              <h2>Search For A Verb</h2>
-            </div>
-            <SearchBar />
-            <div className={styles.buttonContainer}>
-              <button>One Verb</button>
+			<Page>
+				<main>
+					<div>
+						<div className={styles.mainTitleContainer}>
+							<div style={{ width: '350px' }}>
+								<MainTitle />
+							</div>
+						</div>
+					</div>
+					<div className={styles.searchContainer}>
+						<div className={styles.searchTitleContainer}>
+							<h2>Search For A Verb</h2>
+						</div>
+						<SearchBar />
+						<div className={styles.buttonContainer}>
+							<button>One Verb</button>
 
-              <button>All Verb</button>
-            </div>
-          </div>
-          <div></div>
-        </main>
-      </Page>
-      <Footer />
-    </>
-  );
+							<button>All Verb</button>
+						</div>
+					</div>
+					<div></div>
+				</main>
+			</Page>
+			<Footer />
+		</>
+	);
 }
 
 export default Mainpage;
