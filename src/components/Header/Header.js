009f8327cb2eb97edf6589e6256fe2ff9cff335a
Giga Uchaneishvili <g.uchaneishvili@gmail.com>
2023-05-28 18:10:22 +0200

    navbar onclick and cursor pointer is added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,33 +1,48 @@
 /** @format */
 
-import React, { useState } from 'react';
-import styles from './Header.module.css';
-import { DoubleLeftOutlined, DoubleRightOutlined } from '@ant-design/icons';
+import React, { useState } from "react";
+import styles from "./Header.module.css";
+import { DoubleLeftOutlined, DoubleRightOutlined } from "@ant-design/icons";
 
 // import { SearchBarIcon } from '../../icons/searchBarIcon';
 // import { Form, Button } from 'antd';
-import { Logo } from '../../icons/logo';
+import { Logo } from "../../icons/logo";
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 function Header(props) {
-	const [searchValue, setSearchValue] = useState('');
+	const [searchValue, setSearchValue] = useState("");
 	const navigate = useNavigate();
 	return (
 		<div className={styles.nav}>
 			<div
 				className={styles.logoContainer}
 				onClick={() => {
-					navigate('/');
+					navigate("/");
 				}}>
 				<div>
 					<Logo />
 				</div>
 			</div>
 			<ul className={styles.navUl}>
-				<li>PUBLICATIONS</li>
-				<li>PROJECT DESCRIPTION</li>
-				<li>TRANSLITERATION TABLE</li>
+				<li
+					onClick={() => {
+						navigate("/publications");
+					}}>
+					PUBLICATIONS
+				</li>
+				<li
+					onClick={() => {
+						navigate("/project-description");
+					}}>
+					PROJECT DESCRIPTION
+				</li>
+				<li
+					onClick={() => {
+						navigate("/transliteration-table");
+					}}>
+					TRANSLITERATION TABLE
+				</li>
 			</ul>
 
 			{/* {props.search && (
@@ -50,40 +65,40 @@ function Header(props) {
 				</div>
 			)} */}
 			<div className={styles.menuContainer}>
-				{window.location.pathname.includes('allVerb') && (
+				{window.location.pathname.includes("allVerb") && (
 					<button
 						style={{
-							background: '#8E0727',
-							color: 'inherit',
-							cursor: 'pointer',
-							outline: 'none',
-							boxShadow: 'false',
-							height: '40px',
-							width: '120px',
-							border: '1px solid #FFFFFF',
-							borderRadius: '10px',
+							background: "#8E0727",
+							color: "inherit",
+							cursor: "pointer",
+							outline: "none",
+							boxShadow: "false",
+							height: "40px",
+							width: "120px",
+							border: "1px solid #FFFFFF",
+							borderRadius: "10px",
 						}}
-						onClick={() => navigate('/detailPage/tamashi')}>
+						onClick={() => navigate("/detailPage/tamashi")}>
 						<DoubleLeftOutlined />
-						{'One Verb'}
+						{"One Verb"}
 					</button>
 				)}
 
-				{window.location.pathname.includes('detailPage') && (
+				{window.location.pathname.includes("detailPage") && (
 					<button
 						style={{
-							background: '#8E0727',
-							color: 'inherit',
-							cursor: 'pointer',
-							outline: 'none',
-							boxShadow: 'false',
-							height: '40px',
-							width: '120px',
-							border: '1px solid #FFFFFF',
-							borderRadius: '10px',
+							background: "#8E0727",
+							color: "inherit",
+							cursor: "pointer",
+							outline: "none",
+							boxShadow: "false",
+							height: "40px",
+							width: "120px",
+							border: "1px solid #FFFFFF",
+							borderRadius: "10px",
 						}}
-						onClick={() => navigate('/allVerb')}>
-						{'All Verbs'} <DoubleRightOutlined />
+						onClick={() => navigate("/allVerb")}>
+						{"All Verbs"} <DoubleRightOutlined />
 					</button>
 				)}
 			</div>
